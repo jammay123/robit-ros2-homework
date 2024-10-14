7ba9fd7e461929a043419a0bde851f7c88c33cf6
jammay123 <jmha08@naver.com>
2024-10-14 21:24:57 +0900

    HW3
    unfin_fix

--- HW3/turtle_control/src/mainwindow.cpp
+++ HW3/turtle_control/src/mainwindow.cpp
@@ -10,20 +10,18 @@ MainWindow::MainWindow(QWidget* parent)
     ui->setupUi(this);
     QIcon icon(":/image/images/icon.png");
 
-    // ROS2 노드 생성
     node_ = rclcpp::Node::make_shared("qt_turtle_control_node");
-    publisher_ = node_->create_publisher<ros2_custom_interface::msg::Hw3>("turtle1/cmd_vel", 10); // 퍼블리셔 타입 변경
+    publisher_ = node_->create_publisher<ros2_custom_interface::msg::Hw3>("turtle1/cmd_vel", 10); 
 
     setWindowIcon(icon);
 
-    // QTimer 설정: 주기적으로 ROS 이벤트 처리
     connect(&ros_timer_, &QTimer::timeout, this, &MainWindow::process_ros_events);
-    ros_timer_.start(10); // 10ms마다 호출
+    ros_timer_.start(10); 
 }
 
 MainWindow::~MainWindow()
 {
-    // ROS2 종료는 main.cpp에서 처리
+
     delete ui;
 }
 
@@ -35,32 +33,32 @@ void MainWindow::process_ros_events()
 void MainWindow::on_forward_btn_clicked()
 {
     auto msg = ros2_custom_interface::msg::Hw3();
-    msg.linear_x = 2.0; // 앞으로 이동
-    msg.angular_z = 0.0; // 회전 없음
+    msg.linear_x = 2.0; 
+    msg.angular_z = 0.0;
     publisher_->publish(msg);
 }
 
 void MainWindow::on_backward_btn_clicked()
 {
     auto msg = ros2_custom_interface::msg::Hw3();
-    msg.linear_x = -2.0; // 뒤로 이동
-    msg.angular_z = 0.0; // 회전 없음
+    msg.linear_x = -2.0; 
+    msg.angular_z = 0.0;
     publisher_->publish(msg);
 }
 
 void MainWindow::on_turn_right_btn_clicked()
 {
     auto msg = ros2_custom_interface::msg::Hw3();
-    msg.linear_x = 0.0; // 이동 없음
-    msg.angular_z = -1.0; // 우회전
+    msg.linear_x = 0.0; 
+    msg.angular_z = -1.0; 
     publisher_->publish(msg);
 }
 
 void MainWindow::on_turn_left_btn_clicked()
 {
     auto msg = ros2_custom_interface::msg::Hw3();
-    msg.linear_x = 0.0; // 이동 없음
-    msg.angular_z = 1.0; // 좌회전
+    msg.linear_x = 0.0;
+    msg.angular_z = 1.0;
     publisher_->publish(msg);
 }
 
